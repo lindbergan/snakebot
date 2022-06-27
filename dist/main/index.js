41f0454c9bd44eee7cbf7cf0ee7dee3eb27c6a27
Adrian Lindberg <adrianstaffaslindberg@gmail.com>
2022-06-27 18:03:36 +0200

    Add gitignore

--- dist/main/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const game_1 = require("./game");
-const WIDTH = 6, HEIGHT = 6, NR_OF_SNAKES = 2;
-const game = new game_1.Game(WIDTH, HEIGHT, NR_OF_SNAKES);
-game.start();
